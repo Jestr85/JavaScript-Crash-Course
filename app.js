7080f44f8ed2800a00a0b3ddcb1f427d0eb44f60
Jestr85 <jestr85x@gmail.com>
2025-07-08 22:36:06 -0700

    learned functions

--- app.js
+++ app.js
@@ -5,3 +5,26 @@ for (let i = 0; i <= str.length; i++) {
 }
 
 // for loop -  for printing every character of a string //
+
+function welcomePersonToFES(firstName, lastName) {
+    console.log(`Welcome to FES, ${firstName}, ${lastName}`)
+}
+welcomePersonToFES('David', 'Bragg');
+welcomePersonToFES('John', 'Doe');
+
+// calling a function //
+
+// function convertCelsiusToFahrenheit(celsius)
+//    return celsius * 1.8 + 32
+
+
+// console.log(convertCelsiusToFahrenheit(0))
+// console.log(convertCelsiusToFahrenheit(10))
+// console.log(convertCelsiusToFahrenheit(30))
+
+// 2 ways to call functions //
+
+const convertCelsiusToFahrenheit = () => {
+    return 'FES'
+}
+console.log(convertCelsiusToFahrenheit(0))
